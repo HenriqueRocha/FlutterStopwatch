64e11ece790dcc34161c26d7a90384f5bb5fa704
Henrique Rocha <hmrocha@gmail.com>
2019-11-09 16:30:36 +0100

    Alternate between play and pause.

--- lib/main.dart
+++ lib/main.dart
@@ -44,16 +44,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  IconData icon = Icons.play_arrow;
+
   int _counter = 0;
 
-  void _incrementCounter() {
+  void start() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (icon == Icons.play_arrow)
+        icon = Icons.pause;
+      else
+        icon = Icons.play_arrow;
     });
   }
 
@@ -99,9 +99,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: start,
         tooltip: 'Increment',
-        child: Icon(Icons.play_arrow),
+        child: Icon(icon),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
